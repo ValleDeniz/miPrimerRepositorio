2d7994c10c63ddbed5940c6159a2042949f9317c
ValleDeniz <usr2673@salesianos-lacuesta.net>
2021-02-11 13:59:37 +0000

    creado varios ficheros con extencion diferentes

---
+++ primer.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class primer {
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+
+        int palabra;
+         System.out.println("escribe algo");
+         palabra = teclado.nextInt();
+
+         System.out.println(palabra);
+
+     teclado.close();
+
+}
